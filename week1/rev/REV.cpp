a035b872f0f78c7c38c7d8dd7adb36a20037f821
Jakub G <j.glowacki@samsung.com>
2023-07-03 16:33:35 +0200

    rev 100

--- week1/rev/REV.cpp
+++ week1/rev/REV.cpp
@@ -3,16 +3,15 @@
 class REV : public IREV {
    public:
     int bitReverse(int value) {
-        unsigned int res = 0;
         value = *(unsigned int*)&value;
 
+        value = ((value & 0xffff0000) >> 16) | ((value & 0x0000ffff) << 16);
+        value = ((value & 0xff00ff00) >> 8) | ((value & 0x00ff00ff) << 8);
+        value = ((value & 0xf0f0f0f0) >> 4) | ((value & 0x0f0f0f0f) << 4);
+        value = ((value & 0xcccccccc) >> 2) | ((value & 0x33333333) << 2);
+        value = ((value & 0xaaaaaaaa) >> 1) | ((value & 0x55555555) << 1);
 
-        for (unsigned int i = 0; i<32 && value; i++) {
-            res |= (value & 1) << (31 - i);
-            value >>= 1;
-        }
-
-        return *(int*)&res;
+        return *(int*)&value;
     }
 };
 
