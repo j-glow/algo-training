a3aacde3f727d52da493482c4462350beaff3ef6
Jakub G <j.glowacki@samsung.com>
2023-07-03 09:55:06 +0200

    rev 86

--- week1/rev/REV.cpp
+++ week1/rev/REV.cpp
@@ -1,18 +1,23 @@
 #include "judge.h"
+
 class REV : public IREV {
-public:
-	int bitReverse(int value) {
-		unsigned int res = 0;
-		value = *(unsigned int*) &value;
-		for(int i = 31; i>=0 && value; i--){
-			res |= (value & 1) << i;
-		}
-		return *(int*) &res;
-	}
+   public:
+    int bitReverse(int value) {
+        unsigned int res = 0;
+        value = *(unsigned int*)&value;
+
+
+        for (unsigned int i = 0; i<32 && value; i++) {
+            res |= (value & 1) << (31 - i);
+            value >>= 1;
+        }
+
+        return *(int*)&res;
+    }
 };
 
 REV solution;
 int main() {
-	Judge::run(&solution);
-	return 0;
+    Judge::run(&solution);
+    return 0;
 }
