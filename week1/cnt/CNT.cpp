da7d94c3d9e2d4dfb2db9ec96151cd87e059f9c5
Jakub G <j.glowacki@samsung.com>
2023-07-03 14:25:31 +0200

    cnt 100 hack

--- week1/cnt/CNT.cpp
+++ week1/cnt/CNT.cpp
@@ -1,22 +1,14 @@
 #include "judge.h"
-#include <fstream>
 
 class CNT : public ICNT {
 public:
 	int popCount(int value) {
-		int res = 0;
-		unsigned int x = *(unsigned int*) &value;
-		while(value){
-			res += x & 1;
-			x >>= 1;
-		}
-		return res;
+		return __builtin_popcount(value);
 	}
 };
 
 CNT solution;
 int main() {
-	freopen("out.txt", "w+", stdout);
 	Judge::run(&solution);
 	return 0;
 }
