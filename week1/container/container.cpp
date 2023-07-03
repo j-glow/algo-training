4dfe0cce2ff6fb09dc34e50ae287f88721e4cf98
Jakub G <j.glowacki@samsung.com>
2023-07-03 10:41:20 +0200

    fix index out of bounds

--- week1/container/container.cpp
+++ week1/container/container.cpp
@@ -4,28 +4,35 @@
 
 class Container : public IContainer {
     bool* m_values;
-    int m_size;
+    int m_size = 0;
 
    public:
     void init(int size) {
-        m_values = (bool*)calloc(size, sizeof(int));
         m_size = size;
+        if (m_size > 0)
+            m_values = (bool*)malloc(m_size * sizeof(bool));
+        for(int i = 0; i<m_size; i++)
+            m_values[i]=false;
     }
+
     bool add(int a) {
-        if (a > m_size) return false;
-        
+        if (a >= m_size || a < 0 || m_values == NULL) return true;
+
         if (!m_values[a]) {
-            return m_values[a] = true;
+            m_values[a] = true;
+            return true;
         }
         return false;
     }
+
     bool contains(int a) {
-        if (a > m_size) return false;
+        if (a >= m_size || a < 0 || m_values == NULL) return true;
 
         return m_values[a];
     }
+
     bool remove(int a) {
-        if (a > m_size) return false;
+        if (a >= m_size || a < 0 || m_values == NULL) return true;
 
         if (m_values[a]) {
             m_values[a] = false;
