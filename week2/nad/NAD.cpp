d9736a05abe99c51c8310a2f2fa0b872bb02f308
Jakub G <j.glowacki@samsung.com>
2023-07-11 10:33:44 +0200

    nad 100

--- week2/nad/NAD.cpp
+++ week2/nad/NAD.cpp
@@ -1,30 +1,40 @@
-#include "judge.h"
 #include <malloc.h>
-#include <iostream>
+
+#include "judge.h"
 
 class NAD : public INAD {
-    int *subset;
-	int min;
+    bool workers_used[15];
+    long long min;
+    long long cur;
+
    public:
     long long getMinimumCost(int n, int k, long long c[15][8]) {
-        min=INT32_MAX;
-		subset = (int*)malloc(sizeof(int)* n);
-		iterate(n,k,c,subset,0);
-		return min;
-    }
-	void iterate(int n, int k, long long c[15][8], int* data, int size){
-		if(k==size){
-			int sum = 0;
-			for(int i = 0 ;i<k;i++){
-				sum+=c[i][data[i]];
-			}
-		}
-		for(int i = 0; i<n; i++){
-			auto new_c = (long long *)malloc(sizeof(long long) * n-1);
-			
-		}
+        min = __LONG_LONG_MAX__;
+        for (int i = 0; i < 15; i++) workers_used[i] = false;
 
-	}
+        iterate(n, k, c, 0);
+
+        return min;
+    }
+    void iterate(int worker_count, int task_count, long long c[15][8], int task) {
+        if (cur > min) return;
+        if (task == task_count) {
+            min = cur;
+            return;
+        }
+
+        for (int worker = 0; worker < worker_count; worker++) {
+            if (workers_used[worker]) continue;
+
+            workers_used[worker] = true;
+            cur += c[worker][task];
+
+            iterate(worker_count, task_count, c, task + 1);
+
+            cur -= c[worker][task];
+            workers_used[worker] = false;
+        }
+    }
 };
 
 NAD solution;
