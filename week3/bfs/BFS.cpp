59d2f639b39a8090c09bcd699db4c3044a785a64
Jakub G <j.glowacki@samsung.com>
2023-07-13 11:08:19 +0200

    bfs 100

--- week3/bfs/BFS.cpp
+++ week3/bfs/BFS.cpp
@@ -1,5 +1,12 @@
 #include "judge.h"
-#include <cstdio>
+// #include <cstdio>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <string>
+#include <unordered_map>
+#include <vector>
+
 using namespace std;
 
 class BFS : public iBFS {
@@ -11,12 +18,54 @@ class BFS : public iBFS {
         }
         printf("QUESTION: %s -> %s\n", start, end);
     }
-  public:
+
+   public:
     int solve(int n, int m, const char *start, const char *end, const char **a,
               const char **b) {
         // printTestCase(n, m, start, end, a, b);
-        int result = 0;
-        return result;
+
+        // PREPARE DATA
+        string sstart{start};
+        string send{end};
+        if (sstart == send) return 0;
+        string sa[m];
+        string sb[m];
+        for (int i = 0; i < m; i++) {
+            sa[i] = string(a[i]);
+            sb[i] = string(b[i]);
+        }
+        auto routes = create_map(m, sa, sb);
+        unordered_map<string, int> shortest_path;
+        queue<string> order;
+
+        // PREPARE LOOP
+        string city = sstart;
+        shortest_path[city] = 0;
+        while (true) {
+            for (auto i : routes[city]) {
+                if (shortest_path.count(i))
+                    continue;
+                else {
+                    shortest_path[i] = shortest_path[city] + 1;
+                    order.push(i);
+                }
+            }
+            if (order.empty()) break;
+            city = order.front();
+            order.pop();
+        }
+        if (shortest_path[send])
+            return shortest_path[send];
+        else
+            return -1;
+    }
+
+    map<string, vector<string>> create_map(int m, string a[], string b[]) {
+        map<string, vector<string>> routes;
+        for (int i = 0; i < m; i++) {
+            routes[a[i]].push_back(b[i]);
+        }
+        return routes;
     }
 };
 
